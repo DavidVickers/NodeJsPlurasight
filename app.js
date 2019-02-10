d54c165bb566cfaa274047d470f63d7e40988dde
David <David.vickers@icloud.com>
2019-02-10 13:41:50 -0500

    debug

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require('express');
-
+var chalk = require('chalk');
+var debug = require('debug')('app')
 var app = express();
 app.get('/',function(req, res){
     res.send('hello from my  library app');
 })
 
 app.listen(3000,function(){
-    console.log('listening on port 3000');
-});
+    console.log(`listening on port ${chalk.green('3000')}`);
+})
