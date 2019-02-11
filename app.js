c738716a02e7c871b722001ca3f99b9245f1defa
David <David.vickers@icloud.com>
2019-02-10 20:34:53 -0500

    corrected lint errors

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/Public/')));
-app.get('/',function(req, res){
-    res.sendFile(path.join(__dirname, '/views/index.html'));
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
 
-app.listen(3000,function(){
-    console.log(`listening on port ${chalk.green('3000')}`);
-})
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
+});
